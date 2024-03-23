59c6aa8b2618dbb661ae667d5e1418c33f986daa
David Mansourian <mada20sv@student.ju.se>
2024-03-23 11:13:44 +0100

    User in separate file

---
+++ New-GitHub/Models/Data/User.swift
@@ -0,0 +1,25 @@
+//
+//  User.swift
+//  New-GitHub
+//
+//  Created by David Mansourian on 2024-03-23.
+//
+
+import Foundation
+
+public struct User: Codable, Identifiable, Hashable {
+    public let id: Int?
+    let username: String
+    let avatarUrl: String
+    let url: String
+    let reposUrl: String
+    let type: String
+    var avatarImageData: Data?
+    
+    enum CodingKeys: String, CodingKey {
+        case username = "login"
+        case id, type, url
+        case avatarUrl = "avatar_url"
+        case reposUrl = "repos_url"
+    }
+}
--- New-GitHub/Models/Data/UserSearchResult.swift
+++ New-GitHub/Models/Data/UserSearchResult.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct UserSearchResult: Codable, Equatable {
+public struct UserSearchResult: Codable, Hashable {
     let resultsCount: Int
     let incompleteResults: Bool
     let users: [User]
@@ -16,22 +16,5 @@ public struct UserSearchResult: Codable, Equatable {
         case resultsCount = "total_count"
         case incompleteResults = "incomplete_results"
         case users = "items"
-    }
-}
-
-public struct User: Codable, Identifiable, Hashable, Equatable {
-    public let id: Int?
-    let username: String
-    let avatarUrl: String
-    let url: String
-    let reposUrl: String
-    let type: String
-    var avatarImageData: Data?
-    
-    enum CodingKeys: String, CodingKey {
-        case username = "login"
-        case id, type, url
-        case avatarUrl = "avatar_url"
-        case reposUrl = "repos_url"
     }
 }
